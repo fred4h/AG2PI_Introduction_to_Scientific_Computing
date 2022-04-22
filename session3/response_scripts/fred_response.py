6a307d54511958fa0ca3ea562be8332ab4eecfa5
fred4h <fred4h@Fred4h.localdomain>
2022-04-22 14:22:03 -0300

    Freds comments

---
+++ session3/response_scripts/fred_response.py
@@ -0,0 +1,2 @@
+print("""This course was a fantastic experience for me, it is a pitty it is so short! But thank you very much indeed for the experience! I'm looking forward to having new courses with you guys, you are very clear in your speach and the message is conveyed successfully! Do you know any other online open course of yours on that that I could try to attend?
+Thanks again for the course, that was surely worthwhile! Fred (fred-h@hotmail.com)")""")
